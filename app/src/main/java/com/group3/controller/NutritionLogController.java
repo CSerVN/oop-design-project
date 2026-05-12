548124dccc4346159621bb65ce02ffb76c79ded1
huyn80303-bit <huyn80303@gmail.com>
2026-05-13 00:01:16 +0700

    Update NutritionLogController.java

--- app/src/main/java/com/group3/controller/NutritionLogController.java
+++ app/src/main/java/com/group3/controller/NutritionLogController.java
@@ -1,5 +1,16 @@
 package com.group3.controller;
 
-public class NutritionLogController {
+import com.group3.model.Nutrition;
+import com.group3.model.OpenFoodFactsAPI;
 
+public class NutritionLogController {
+private final OpenFoodFactsAPI openFoodFactsAPI = new OpenFoodFactsAPI();
+  public Nutrition lookupNutrition(String productName) throws Exception {
+        return openFoodFactsAPI.fetchNutritionData(productName);
+    }
+  public void saveNutritionLog(Nutrition nutrition) {
+        // TODO: Hoan implement phần này (JsonLogDatabase)
+        System.out.println("Saved: " + nutrition.getFoodName()
+            + " | Cal: " + nutrition.getCalories());
+    }
 }
